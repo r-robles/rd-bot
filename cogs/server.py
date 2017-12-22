2b606b9d083fd25c66446dc2ecf6075dbb7e8e69
r-robles <rfrobles4@gmail.com>
2017-12-21 17:02:39 -1000

    Add serverinfo command

---
+++ cogs/server.py
@@ -0,0 +1,32 @@
+import discord
+from discord.ext import commands
+
+class Server:
+    """Server related commands."""
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    @commands.guild_only()
+    async def serverinfo(self, ctx):
+        """Get information about the server."""
+        guild = ctx.guild
+        embed = discord.Embed(title=guild.name, colour=0x117711)
+        embed.add_field(name='ID', value=guild.id)
+        embed.add_field(name='Server Region', value=guild.region)
+        embed.add_field(name='Members', value=guild.member_count)
+        voice = len(guild.voice_channels)
+        text = len(guild.text_channels)
+        embed.add_field(name='Channels',
+                        value=f'{voice + text} channels: {voice} voice, {text} text')
+        embed.add_field(name='Owner', value=f'{guild.owner.name}#{guild.owner.discriminator}')
+        roles = list(map(lambda x: x.name if x.name != '@everyone' else '@\u200beveryone',
+                         guild.roles))
+        embed.add_field(name='Roles', value=', '.join(roles))
+        create_time = guild.created_at.strftime('%B %d, %Y at %H:%M:%S UTC')
+        embed.set_footer(text=f'Created on {create_time}')
+        embed.set_thumbnail(url=guild.icon_url)
+        await ctx.send(embed=embed)
+
+def setup(bot):
+    bot.add_cog(Server(bot))
