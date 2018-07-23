c84764a905ec953c4af8559eb7101608d9453e19
r-robles <rfrobles4@gmail.com>
2018-07-22 20:03:11 -1000

    Add 8ball command

--- cogs/fun.py
+++ cogs/fun.py
@@ -1,3 +1,4 @@
+import random
 import discord
 from discord.ext import commands
 from utils.messages import ColoredEmbed
@@ -22,6 +23,34 @@ class Fun:
     async def lenny(self, ctx):
         """( ͡° ͜ʖ ͡°)"""
         await ctx.send('( ͡° ͜ʖ ͡°)')
+
+    @commands.command(name='8ball')
+    async def eight_ball(self, ctx, *, question):
+        """Ask the magic 8 ball."""
+        if not question.endswith('?'):
+            await ctx.send('That\'s not a question!')
+            return
+        responses = ['It is certain.',
+                     'It is decidedly so.',
+                     'Without a doubt.',
+                     'Yes - definitely.',
+                     'You may rely on it.',
+                     'As I see it, yes.',
+                     'Most likely.',
+                     'Outlook good.',
+                     'Yes.',
+                     'Signs point to yes.',
+                     'Reply hazy, try again',
+                     'Ask again later.',
+                     'Better not tell you now.',
+                     'Cannot predict now.',
+                     'Concentrate and ask again.',
+                     'Don\'t count on it.',
+                     'My reply is no.',
+                     'My sources say no.',
+                     'Outlook not so good.',
+                     'Very doubtful.']
+        await ctx.send(f'You asked: **{question}**\n:8ball: {random.choice(responses)}')
 
 
 def setup(bot):
