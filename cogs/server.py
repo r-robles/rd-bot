02bd967b764accdcbc0f332da012ad1cd4ef192a
r-robles <rfrobles4@gmail.com>
2019-01-30 18:26:25 -1000

    Update prefix commands

--- cogs/server.py
+++ cogs/server.py
@@ -72,6 +72,91 @@ class Server:
         embed.add_field(name='Activity', value=member.activity)
         await ctx.send(embed=embed)
 
+    @commands.group()
+    async def prefix(self, ctx):
+        """Manage the prefixes for invoking commands.
+        """
+        if ctx.invoked_subcommand is None:
+            help_command = self.bot.get_command('help')
+            await ctx.invoke(help_command, 'prefix')
+
+    @prefix.command(name='list')
+    async def prefix_list(self, ctx):
+        """List the prefixes for this server.
+        """
+        prefixes = map(lambda x: f'`{x}`', self.bot.prefixes[ctx.guild.id])
+        await ctx.send(f'Here are the prefixes for this server:\n{", ".join(prefixes)}')
+
+    async def _update_prefixes(self, guild_id, prefixes):
+        """Update the database and cache with the new prefixes for the guild.
+
+        Args:
+            guild_id (int): the id of the guild to update
+            prefixes (str[]): the updated list of prefixes
+        """
+        connection = await self.bot.database.acquire()
+        async with connection.transaction():
+            query = "update prefixes set prefixes = $1 where guild_id = $2;"
+            await connection.execute(query, prefixes, guild_id)
+        await self.bot.database.release(connection)
+
+        self.bot.prefixes[guild_id] = prefixes
+
+    @commands.has_permissions(manage_guild=True)
+    @prefix.command(name='add')
+    async def prefix_add(self, ctx, prefix: str):
+        """Add a new prefix to this server.
+
+        If you want your prefix to contain spaces in the middle or end,
+        then the prefix should be enclosed in double quotes.
+
+        Required Permissions:
+            Manage Guild
+
+        Args:
+            prefix: the prefix to add
+        """
+        prefix_to_add = prefix.lstrip(' ')
+
+        guild_id = ctx.guild.id
+        current_prefixes = self.bot.prefixes[guild_id]
+
+        if prefix_to_add in current_prefixes:
+            return await ctx.send(f'`{prefix}` is already a prefix for this server!')
+        if len(prefix_to_add) == 0:
+            return await ctx.send(f'You must enter a prefix to add!')
+
+        current_prefixes.append(prefix_to_add)
+        await self._update_prefixes(guild_id, current_prefixes)
+
+        formatted_prefixes = map(lambda x: f'`{x}`', current_prefixes)
+        await ctx.send(f'Here are the new prefixes for this server:\n{", ".join(formatted_prefixes)}')
+
+    @commands.has_permissions(manage_guild=True)
+    @prefix.command(name='remove', aliases=['delete'])
+    async def prefix_remove(self, ctx, prefix: str):
+        """Remove a prefix from this server.
+
+        For prefixes that contain spaces in the middle or at the end,
+        enclose the prefix in double quotes.
+
+        Args:
+            prefix: the prefix to remove
+        """
+        guild_id = ctx.guild.id
+        current_prefixes = self.bot.prefixes[guild_id]
+
+        if prefix not in current_prefixes:
+            return await ctx.send(f'`{prefix}` is not a prefix for this server!')
+        if len(current_prefixes) == 1:
+            return await ctx.send(f'You must have at least 1 prefix remaining for this server.')
+
+        current_prefixes.remove(prefix)
+        await self._update_prefixes(guild_id, current_prefixes)
+
+        formatted_prefixes = map(lambda x: f'`{x}`', current_prefixes)
+        await ctx.send(f'Here are the new prefixes for this server:\n{", ".join(formatted_prefixes)}')
+
 
 def setup(bot):
     bot.add_cog(Server(bot))
